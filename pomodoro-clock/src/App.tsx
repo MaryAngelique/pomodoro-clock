4d058018eb0545525b61ff51b09502a17f4bb110
Angelique Abacajan <ma_utsey@yahoo.com>
2023-09-05 18:24:28 -0400

    Implemented App component with base structure to render pomodoro clock ui

--- pomodoro-clock/src/App.tsx
+++ pomodoro-clock/src/App.tsx
@@ -1,35 +1,79 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import { DisplayState } from "./helper";
+import TimeSetter from "./TimeSetter";
+import Display from "./Display";
+import AlarmSound from "./assets/AlarmSound.mp3";
+import './App.css';
+
+const defaultBreakTime = 5 * 60;
+const defaultSessionTime = 25 * 60;
+const min = 60;
+const max = 60 * 60;
+const interval = 60;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [breakTime, setBreakTime] = useState(defaultBreakTime);
+  const [sessionTime, setSessionTime] = useState(defaultSessionTime);
+  const [displayState, setDisplayState] = useState<DisplayState>({
+    time: sessionTime,
+    timeType: "Session",
+    timerRunning: false,
+  });
+
+  const reset = () => {
+    setBreakTime(defaultBreakTime);
+    setSessionTime(defaultSessionTime);
+    setDisplayState({
+      time: defaultSessionTime,
+      timeType: "Session",
+      timerRunning: false,
+    });
+    const audio = document.getElementById("beep") as HTMLAudioElement;
+    audio.pause();
+    audio.currentTime = 0;
+  };
+
+  const startStop = () => {
+    setDisplayState((prev) => ({
+      ...prev,
+      timerRunning: !prev.timerRunning,
+    }));
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="clock">
+      <div className="setters">
+        <div className="break">
+          <h4 id="break-label">Break Length</h4>
+          <TimeSetter
+            time={breakTime}
+            setTime={setBreakTime}
+            min={min}
+            max={max}
+            interval={interval}
+            type="break"
+          />
+        </div>
+        <div className="session">
+          <h4 id="session-label">Session Length</h4>
+          <TimeSetter
+            time={sessionTime}
+            setTime={setSessionTime}
+            min={min}
+            max={max}
+            interval={interval}
+            type="session"
+          />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <Display
+        displayState={displayState}
+        reset={reset}
+        startStop={startStop}
+      />
+      <audio id="beep" src={AlarmSound} />
+    </div>
+  );
 }
 
-export default App
+export default App;
